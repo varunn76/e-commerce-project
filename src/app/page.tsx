8db35ee61e8fb659de140a31301360f24c2ea5b1
varun <vk465396@gmail.com>
2025-11-29 22:45:25 +0530

    small fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,18 +7,17 @@ import { useRouter } from "next/navigation";
 const Home = () => {
   const { user } = useAuth();
   const router = useRouter();
-  console.log("uss", user);
 
   return (
     <div
-      className={`min-h-screen px-4  py- bg-background font-sans ${
-        user?.role === "admin" ? "my-10" : "my-24"
+      className={`min-h-screen lg:max-w-6xl px-4 mx-auto items-center flex flex-col lg:items-end  py-6 bg-background font-sans ${
+        user?.role === "admin" ? "my-16" : "my-24"
       }`}
     >
       {user?.role === "admin" && (
         <button
           onClick={() => router.push("/admin/products")}
-          className="mb-4 w-full flex justify-end md:w-auto px-4 py-2 rounded-md bg-primary text-white hover:bg-primary/80 transition"
+          className="mb-4  md:w-auto px-4 py-2 rounded-md bg-primary text-white hover:bg-primary/80 transition"
         >
           Admin Panel
         </button>
